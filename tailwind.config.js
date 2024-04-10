2d94d11dc847c9aaa66c65d2637c0f1cbee2928c
Rita Solà <ritasolacanals@gmail.com>
2024-04-11 00:28:24 +0200

    add colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueLight: "#ADD8E6",
+        blue: "#6495ED",
+        black: "#000000",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
